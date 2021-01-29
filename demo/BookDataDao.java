d6570cb2fd993ffcd4f6ef50a76de71fc65ffe07
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2021-01-29 13:45:13 +0900

    initial

---
+++ demo/BookDataDao.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BookDataDao extends Serializable {
+
+    public List<BookData> search(String genre, String author, String title);
+}
